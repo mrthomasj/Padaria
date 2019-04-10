6864c3f13359fe8e944240634b7f9f693f03cf38
mrthomasj <nekosephi@gmail.com>
2019-04-10 18:56:49 -0300

    First Changes

---
+++ Padaria/UserForm.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Padaria
+{
+    partial class UserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblNm = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtMatric = new System.Windows.Forms.TextBox();
+            this.lblMatric = new System.Windows.Forms.Label();
+            this.lvlDtNasc = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.maskedTextBox1);
+            this.panel1.Controls.Add(this.lvlDtNasc);
+            this.panel1.Controls.Add(this.txtMatric);
+            this.panel1.Controls.Add(this.lblMatric);
+            this.panel1.Controls.Add(this.txtCpf);
+            this.panel1.Controls.Add(this.lblCpf);
+            this.panel1.Controls.Add(this.txtNome);
+            this.panel1.Controls.Add(this.lblNm);
+            this.panel1.Location = new System.Drawing.Point(68, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(796, 256);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblNm
+            // 
+            this.lblNm.AutoSize = true;
+            this.lblNm.Location = new System.Drawing.Point(15, 14);
+            this.lblNm.Name = "lblNm";
+            this.lblNm.Size = new System.Drawing.Size(38, 13);
+            this.lblNm.TabIndex = 0;
+            this.lblNm.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(18, 30);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(192, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtMatric
+            // 
+            this.txtMatric.Location = new System.Drawing.Point(527, 30);
+            this.txtMatric.Name = "txtMatric";
+            this.txtMatric.Size = new System.Drawing.Size(61, 20);
+            this.txtMatric.TabIndex = 5;
+            // 
+            // lblMatric
+            // 
+            this.lblMatric.AutoSize = true;
+            this.lblMatric.Location = new System.Drawing.Point(524, 14);
+            this.lblMatric.Name = "lblMatric";
+            this.lblMatric.Size = new System.Drawing.Size(50, 13);
+            this.lblMatric.TabIndex = 4;
+            this.lblMatric.Text = "Matricula";
+            // 
+            // lvlDtNasc
+            // 
+            this.lvlDtNasc.AutoSize = true;
+            this.lvlDtNasc.Location = new System.Drawing.Point(389, 14);
+            this.lvlDtNasc.Name = "lvlDtNasc";
+            this.lvlDtNasc.Size = new System.Drawing.Size(107, 13);
+            this.lvlDtNasc.TabIndex = 6;
+            this.lvlDtNasc.Text = "Data de Nascimento:";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(392, 30);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 7;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Location = new System.Drawing.Point(232, 14);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(26, 13);
+            this.lblCpf.TabIndex = 2;
+            this.lblCpf.Text = "Cpf:";
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(235, 30);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(111, 20);
+            this.txtCpf.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Cpf:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(235, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(111, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Cpf:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(392, 85);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(111, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(389, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Cpf:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(527, 85);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(111, 20);
+            this.textBox4.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(524, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Cpf:";
+            // 
+            // UserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 611);
+            this.Controls.Add(this.panel1);
+            this.Name = "UserForm";
+            this.Text = "UserForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblNm;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label lvlDtNasc;
+        private System.Windows.Forms.TextBox txtMatric;
+        private System.Windows.Forms.Label lblMatric;
+        private System.Windows.Forms.TextBox txtCpf;
+        private System.Windows.Forms.Label lblCpf;
+        private System.Windows.Forms.TextBox txtNome;
+    }
+}
